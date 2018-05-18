b1527624d2d5b531fedc87b076bba92fdf64134b
桥黎 <cql.ql@qq.com>
2018-05-18 15:16:47 +0800

    注释

--- build/friendly-error.js
+++ build/friendly-error.js
@@ -9,7 +9,7 @@ module.exports = function (webpackConfig) {
     portfinder.basePort = webpackConfig.devServer.port || 8080
     portfinder.getPort((err, port) => {
       webpackConfig.devServer.port = port
-      webpackConfig.devServer.quiet = true // necessary for FriendlyErrorsPlugin
+      webpackConfig.devServer.quiet = true // 取消自带的编译信息。 necessary for FriendlyErrorsPlugin
       webpackConfig.plugins.push(new FriendlyErrorsWebpackPlugin({
         compilationSuccessInfo: {
           messages: [`start: http://${webpackConfig.devServer.host}:${port}`],
