357ddde404439c484a3d81dbae8a991dfb2d1cc7
桥黎 <cql.ql@qq.com>
2018-06-13 13:10:50 +0800

    优化

--- data-mock/server.run.js
+++ data-mock/server.run.js
@@ -1,11 +1,14 @@
 // 数据模拟
 var getIPAdress = require('../build/get-ip-adress')
 var express = require('express')
+var bodyParser = require('body-parser')
 var router = require('./data')
 
 var app = express()
 
-app.use('/Content', express.static('E:\\_work\\sd.pi\\src\\sd.pi.wxweb\\Content'))
+app.use(bodyParser.json()) // for parsing application/json
+
+// app.use('/Content', express.static('E:\\_work\\sd.pi\\src\\sd.pi.wxweb\\Content')) // 静态资源
 
 app.use('/', router)
 var server = app.listen(3003, getIPAdress(), function () {
