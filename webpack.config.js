9dc2f04168a1fb715b5d7779743b4860cf690856
桥黎 <cql.ql@qq.com>
2018-06-13 13:00:46 +0800

    优化

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ webpackConfig.devServer.proxy = [{
   // onProxyReq: function(proxyReq, req, res){
   //   proxyReq.setHeader('Host',`192.168.1.222:3002`)
   //   proxyReq.setHeader('Cookie', `
-  //     .ASPXAUTH=47DB980C8F37CC95918231651123DC05B7DBD3B2082F17CE13FF8435836C6405550374B0C50CEEB3A7DFAE8D55323906CFBDEB8FA1DF40FAB9EC3D114EA3EB3DF69249FBD73B7EB9000AD2437399CD21B32F3BC99236DAF7C462C8216642F001; domain=192.168.1.194; expires=Wed, 08-Nov-2017 06:28:56 GMT; path=/; HttpOnly
+  //     .ASPXAUTH=47DB; domain=192.168.1.194; expires=Wed, 08-Nov-2017 06:28:56 GMT; path=/; HttpOnly
   //   `);
   // },
 
