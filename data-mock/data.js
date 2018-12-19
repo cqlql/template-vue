843e29c1aceb82cfdfefe04d1eab662715edeab2
陈桥黎 <cql.ql@qq.com>
2018-12-19 19:59:16 +0800

    data-mock 支持文件上传

--- data-mock/data.js
+++ data-mock/data.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var router = express.Router()
+var multer = require('multer')
+var upload = multer({ dest: 'e:/uploads/' })
 
 router.get('/GetMessage', function (req, res) {
   res.send({
@@ -9,7 +11,7 @@ router.get('/GetMessage', function (req, res) {
   })
 })
 
-router.post('/file', function (req, res) {
+router.post('/file', upload.single('file'), function (req, res) {
   res.send({
     'status': 200,
     'message': 'ok',
