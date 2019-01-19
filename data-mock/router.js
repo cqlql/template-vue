406f02d9a415fa120233a1319ab33e29f346002f
桥黎 <cql.ql@qq.com>
2019-01-19 14:36:19 +0800

    Update router.js

--- data-mock/router.js
+++ data-mock/router.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var router = express.Router()
+
 // 文件上传
 // var multer = require('multer')
 // var upload = multer({ dest: 'e:/uploads/' })
+// router.post('/file', upload.single('file'), function (req, res) {
+//   res.send({
+//     'status': 200,
+//     'message': 'ok',
+//     'result': {}
+//   })
+// })
 
 // post
 router.post('/Organization/GetDepartments', function (req, res) {
@@ -14,14 +22,5 @@ router.get('/Power/GetClassesInGrades', function (req, res) {
   // console.log(req.body)
   res.send(require('./data/GetClassesInGrades.json')[req.query.PeriodId])
 })
-
-// 文件上传
-// router.post('/file', upload.single('file'), function (req, res) {
-//   res.send({
-//     'status': 200,
-//     'message': 'ok',
-//     'result': {}
-//   })
-// })
 
 module.exports = router
